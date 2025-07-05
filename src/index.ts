984d46c738bca2f253ac29db8b2d16ef16100bc7
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-05 10:42:03 -0300

    Añadiendo funcionalidad para crear las tablas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express,{type Express} from 'express';
 import adminRoutes from './routes/admin.routes';
 import dotenv from 'dotenv';
+import sequelize from './config/database';
+import './models/User';
 
 //Cargando variables de entorno
 dotenv.config();
@@ -11,6 +13,22 @@ const port = process.env.PORT ||3000;
 
 //Añadiendo las rutas de admina
 app.use('/admin',adminRoutes);
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexión a la base de datos exitosa.');
+    
+    // usa force: true solo para borrar y regrear
+    await sequelize.sync({ alter: true }); 
+    console.log('✅ Modelos sincronizados con la base de datos.');
+
+
+  } catch (err) {
+    console.error('❌ Error al conectar o sincronizar:', err);
+  }
+};
+//start();
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde TypeScript y Express!');
