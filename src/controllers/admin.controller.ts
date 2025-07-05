507ee54a8999f3977adc9673a6244db5b2d50abb
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-05 12:43:30 -0300

    Creando el controlador para crear Usuarios

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,17 +1,44 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/User';
+
 
 export const createUser = async (req: Request, res: Response): Promise<Response> => {
-    const { username, password, passwordr } = req.body;
+    try {
 
-    if (!username || !password || !passwordr) {
-        // Aseguramos que esta rama siempre retorne una Response
-        return res.status(400).json({
-            status: "error",
-            message: "The username, password, and passwordr fields are required"
+        const { username, password, passwordr, email } = req.body;
+
+        if (!username || !password || !passwordr ||!email) {
+            
+            return res.status(400).json({
+                status: "error",
+                message: "The username, password,passwordr and email fields are required"
+            });
+        }
+        if(password.trim() !== passwordr.trim()){
+            return res.status(400).json({
+                status: "error",
+                message: "The username, password, and passwordr fields are required"
+            });
+        }
+        //Crear el usuario
+        const hashPassoword = await bcrypt.hash(password,10);
+        await User.create({
+            username:username,
+            password:hashPassoword,
+            email: email
+        })
+        
+        return res.status(201).json({
+                status: "success",
+                message: "User created"
+        });
+    } catch (error) {
+        return res.status(500).json({
+                status: "error",
+                message: "Server Error",
+                error
         });
     }
-
-    // Aseguramos que esta rama también retorne una Response
-    // Si hay más lógica asíncrona aquí (ej. guardar en DB), también deberías retornarla
-    return res.send('usuario creado');
+   
 };
