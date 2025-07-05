5886e2812d3de50631f4121eb06db481fdc84ccc
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-05 12:57:21 -0300

    Agregada comprobacion de si el usuario existe y si es unico

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/User';
+import { isEmailValid } from '../utils/utils';
+import { where } from 'sequelize';
 
 
 export const createUser = async (req: Request, res: Response): Promise<Response> => {
@@ -12,15 +14,31 @@ export const createUser = async (req: Request, res: Response): Promise<Response>
             
             return res.status(400).json({
                 status: "error",
-                message: "The username, password,passwordr and email fields are required"
+                message: "The username, password,passwordr and email fields are required."
             });
+        }
+        if (!isEmailValid(email)) {
+            return res.status(400).json({ error: "Invalid email." });
         }
         if(password.trim() !== passwordr.trim()){
             return res.status(400).json({
                 status: "error",
-                message: "The username, password, and passwordr fields are required"
+                message: "The username, password, and passwordr fields are required."
             });
         }
+
+        //Verificar si el username ya esta en uso
+        const existUser = await User.findOne({where:{username}});
+
+        if(existUser){
+
+            return res.status(400).json({
+                status: "error",
+                message: "The username and email are already in use."
+            });
+            
+        }
+
         //Crear el usuario
         const hashPassoword = await bcrypt.hash(password,10);
         await User.create({
@@ -31,12 +49,12 @@ export const createUser = async (req: Request, res: Response): Promise<Response>
         
         return res.status(201).json({
                 status: "success",
-                message: "User created"
+                message: "User created."
         });
     } catch (error) {
         return res.status(500).json({
                 status: "error",
-                message: "Server Error",
+                message: "Server Error.",
                 error
         });
     }
