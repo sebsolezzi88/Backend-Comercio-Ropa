30898101fcd4c604f7bb92dbc300fec58b13a16d
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-06 10:22:27 -0300

    Creada la funcionalidada para cargar productos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,33 @@
 import { Request, Response } from 'express';
+import { Product } from '../models/Products';
 
 export const addProduct = async (req: Request, res: Response): Promise<Response> =>{
-        return res.send('listo');
+       
+        try {
+            const {name, description,urlImage,categoryId} = req.body;
+
+            if(!name || !description || !urlImage || !categoryId){
+                return res.status(400).json({ 
+                    status: 'error', 
+                    message: 'name, description, urlImage and categoryId are required.';
+                 });
+            }
+
+            //Creamos el producto
+            const newProduct = await Product.create({name,description,urlImage,categoryId});
+
+            return res.status(201).json({
+                    status: "success",
+                    message: "Product created.",
+                    product: newProduct
+            });
+       } catch (error) {
+            return res.status(500).json({
+                    status: "error",
+                    message: "Server Error.",
+                    error
+            });
+       }
 }
 
 export const getProduct = async (req: Request, res: Response): Promise<Response> =>{
