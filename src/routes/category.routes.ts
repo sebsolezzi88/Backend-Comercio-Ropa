8f19ee3a042fa7e3c7cb915eac3bfbfc170e8036
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-05 22:24:42 -0300

    Creacion del middleware para asegurar rutas

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,11 +1,12 @@
 import { Router} from "express";
 import { addCategory, deleteCategory, getCategories, updateCategory } from "../controllers/category.cotroller";
+import { verifyToken } from "../middlewares/authMiddleware";
 
 
 const router = Router();
 
 router.get('/',getCategories); //Obtener categorias
-router.post('/', addCategory); //Crear categoria
+router.post('/',verifyToken ,addCategory); //Crear categoria
 router.delete('/',deleteCategory) //Borra categoria
 router.put('/',updateCategory) //Actualizar categoria
 
