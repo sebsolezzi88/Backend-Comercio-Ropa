ba007b7856aa13b6357188cb348fa2d9afb59274
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-06 22:27:46 -0300

    Funcionalidad para actualizar

--- src/controllers/variant.controller.ts
+++ src/controllers/variant.controller.ts
@@ -49,9 +49,43 @@ export const getVariant = async (req: Request, res: Response): Promise<Response>
 
 export const updateVariant = async (req: Request, res: Response): Promise<Response> =>{
        
+
         try {
            
-            return res.send('listo');
+            const id = req.params.id;
+            const {productId,size,stock,price} = req.body;
+
+            if (!id) {
+              return res.status(400).json({ status: 'error', message: 'Id is required' });
+            }
+
+            if(!productId || !size || !stock || !price){
+                return res.status(400).json({ 
+                    status: 'error', 
+                    message: 'productId, size, stock and price are required.'
+                 });
+            }
+
+            //Buscamos el producto variante
+            const productVariantExist = await ProductVariant.findByPk(id);
+
+            if(!productVariantExist){
+                return res.status(404).json({ status: 'error', message: 'Product variant not found' });
+            }
+
+            productVariantExist.productId = productId;
+            productVariantExist.stock = stock;
+            productVariantExist.size = size;
+            productVariantExist.price = price;
+
+            await productVariantExist.save();
+
+            return res.status(201).json({
+                    status: "success",
+                    message: "Product Variant updated.",
+                    productVariant: productVariantExist
+            });
+            
        } catch (error) {
             return res.status(500).json({
                     status: "error",
@@ -59,6 +93,7 @@ export const updateVariant = async (req: Request, res: Response): Promise<Respon
                     error
             });
        }
+        
 }
 
 export const deleteVariant = async (req: Request, res: Response): Promise<Response> =>{
