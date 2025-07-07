4f7bbd305a8988363778b7cf85b4556499bdb229
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-07 10:59:21 -0300

    funcion para generar codigos de orden

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,13 @@
 export const isEmailValid = (email: string): boolean => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
-};
+}
+
+export const generateOrderCode = (length = 6): string => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
