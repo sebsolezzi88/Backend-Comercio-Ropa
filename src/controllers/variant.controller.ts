5712ba16d6959a0bf0ccfbf835d05ae5101c5e0a
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-06 22:20:26 -0300

    Agregada la funcionalidad para agregar variantes

--- src/controllers/variant.controller.ts
+++ src/controllers/variant.controller.ts
@@ -1,10 +1,29 @@
 import { Request,Response } from "express";
+import { ProductVariant } from "../models/Products";
+
 
 export const addVariant = async (req: Request, res: Response): Promise<Response> =>{
        
         try {
            
-            return res.send('listo');
+            const {productId,size,stock,price} = req.body;
+
+            if(!productId || !size || !stock || !price){
+                return res.status(400).json({ 
+                    status: 'error', 
+                    message: 'productId, size, stock and price are required.'
+                 });
+            }
+
+            //Creamos el producto variante
+            const newProductVariant = await ProductVariant.create({productId,size,stock,price});
+
+            return res.status(201).json({
+                    status: "success",
+                    message: "Product Variant created.",
+                    productVariant: newProductVariant
+            });
+            
        } catch (error) {
             return res.status(500).json({
                     status: "error",
